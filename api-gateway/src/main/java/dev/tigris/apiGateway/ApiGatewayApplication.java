d04fdf99f184fe3cc1febc61acf192c60c98814d
bllexe <bilalykt21@gmail.com>
2023-10-21 21:50:47 +0300

    adding api-gateway server

---
+++ api-gateway/src/main/java/dev/tigris/apiGateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package dev.tigris.apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@EnableHystrix
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class,args);
+    }
+}
